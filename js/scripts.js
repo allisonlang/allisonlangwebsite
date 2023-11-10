440b9cdec26f981e08dada49b9981761ed7a2baa
Anil Kumar krishanshetty <anilbms75@gmail.com>
2023-11-10 19:20:16 +0000

    Fix js script errors

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,5 @@ var test = 'This will not actually create a variable';
 
 //This is a single line comment, started with two slashes
 
-<script>
-alert("Hello");
-</script>
-
 console.log("Welcome to my World!");
 

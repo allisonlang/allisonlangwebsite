b1743fbf886152a5d188295e9d9342432707bca8
allisonlang <allisonlang707@gmail.com>
2023-11-10 17:12:28 +0100

    added js script page

---
+++ js/.scripts.js
@@ -0,0 +1,13 @@
+/* This is a multi-line comment
+Everything in here has no effect.
+var test = 'This will not actually create a variable';
+*/
+
+//This is a single line comment, started with two slashes
+
+<script>
+alert("Hello");
+</script>
+
+console.log("Welcome to my World!");
+

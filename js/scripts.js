a46fa0a35f3e62e862b3ce82dcf741a25643523b
allisonlang <allisonlang707@gmail.com>
2023-11-14 11:32:39 +0100

    removed js test exercise

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,12 @@ var test = 'This will not actually create a variable';
 
 console.log("Welcome to my World!");
 
+var escapedText1 = "He said: \"Yes!\", that's for sure";
+
+var escapedText2 = 'He said: "Yes!", that\'s for sure';
+
+var shortObject = { name: "John", age: 31};
+var emptyObject = {}; // Objects can be empty. You can add properties later
+delete shortObject.name; // Will remove the "name" property
+
+

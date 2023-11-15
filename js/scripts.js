8cf92ed256187dff76a2b185e4cd0b0deca15953
allisonlang <allisonlang707@gmail.com>
2023-11-15 11:25:51 +0100

    added jQuery CDN

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,4 @@
-/* This is a multi-line comment
-Everything in here has no effect.
-var test = 'This will not actually create a variable';
-*/
-
-//This is a single line comment, started with two slashes
-
-console.log("Welcome to my World!");
-
-var escapedText1 = "He said: \"Yes!\", that's for sure";
-
-var escapedText2 = 'He said: "Yes!", that\'s for sure';
-
-var shortObject = { name: "John", age: 31};
-var emptyObject = {}; // Objects can be empty. You can add properties later
-delete shortObject.name; // Will remove the "name" property
-
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" 
+integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" 
+crossorigin="anonymous"></script>
 
